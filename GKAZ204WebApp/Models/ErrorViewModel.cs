07186a7eaffa5c1deb606855a5e2a50725393b99
Abhishek Kant <abhishek@abhishekkant.net>
2020-09-22 01:42:23 +0530

    Add project files.

---
+++ GKAZ204WebApp/Models/ErrorViewModel.cs
@@ -0,0 +1,11 @@
+using System;
+
+namespace GKAZ204WebApp.Models
+{
+    public class ErrorViewModel
+    {
+        public string RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
